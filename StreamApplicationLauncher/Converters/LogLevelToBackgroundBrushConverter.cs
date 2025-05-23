90e118af1c9f418f3abe48761524c004969b20a9
Cory Reid <11530109+coryjreid@users.noreply.github.com>
2025-05-23 13:34:23 -0600

    Update Display of Log Message and Timestamp

---
+++ StreamApplicationLauncher/Converters/LogLevelToBackgroundBrushConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using StreamApplicationLauncher.Models;
+
+namespace StreamApplicationLauncher.Converters;
+
+public class LogLevelToBackgroundBrushConverter : IValueConverter {
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        return value switch {
+            LogLevel.Info => Brushes.Transparent,
+            LogLevel.Warning => new SolidColorBrush(Color.FromArgb(20, 255, 165, 0)), // light orange
+            LogLevel.Error => new SolidColorBrush(Color.FromArgb(30, 255, 0, 0)), // light red
+            _ => Brushes.Transparent
+        };
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
