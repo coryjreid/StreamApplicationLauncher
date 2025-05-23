437c0953e0d36c5e15d3d8d6b9074e7a9d09cf21
Cory Reid <11530109+coryjreid@users.noreply.github.com>
2025-05-23 11:41:08 -0600

    Feature: Runtime Timer

---
+++ LogViewerApp/Models/RuntimeTimer.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+
+namespace LogViewerApp.Models;
+
+public class RuntimeTimer : INotifyPropertyChanged {
+    private readonly DispatcherTimer _timer;
+    private readonly DateTime _startTime;
+
+    private string _durationText = "00:00:00";
+
+    public string DurationText {
+        get => _durationText;
+        private set {
+            if (_durationText != value) {
+                _durationText = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public RuntimeTimer() {
+        _startTime = DateTime.Now;
+
+        _timer = new DispatcherTimer {
+            Interval = TimeSpan.FromSeconds(1)
+        };
+        _timer.Tick += UpdateElapsed;
+        _timer.Start();
+    }
+
+    private void UpdateElapsed(object? sender, EventArgs e) {
+        TimeSpan elapsed = DateTime.Now - _startTime;
+        DurationText = elapsed.ToString(@"hh\:mm\:ss");
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? name = null) {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
--- LogViewerApp/ViewModels/MainViewModel.cs
+++ LogViewerApp/ViewModels/MainViewModel.cs
@@ -12,7 +12,9 @@ public class MainViewModel : INotifyPropertyChanged {
     public ObservableCollection<LogMessage> LogMessages { get; }
     public ScrollState ScrollState { get; } = new ScrollState();
     public ICommand ScrollToBottomCommand { get; }
+    public string DurationText => _runtimeTimer.DurationText;
     private readonly LogManager _logManager;
+    private readonly RuntimeTimer _runtimeTimer;
 
     // Design-time constructor
     public MainViewModel() : this(new DesignTimeLogManager()) {
@@ -21,19 +23,19 @@ public class MainViewModel : INotifyPropertyChanged {
     // Runtime constructor
     public MainViewModel(LogManager logManager) {
         _logManager = logManager;
+        _runtimeTimer = new RuntimeTimer();
         LogMessages = logManager.LogMessages;
         ScrollToBottomCommand = new RelayCommand(ExecuteScrollToBottom, CanScrollToBottom);
         ScrollState.PropertyChanged += (_, _) => { (ScrollToBottomCommand as RelayCommand)?.RaiseCanExecuteChanged(); };
+        _runtimeTimer.PropertyChanged += (_, args) => {
+            if (args.PropertyName == nameof(RuntimeTimer.DurationText)) {
+                OnPropertyChanged(nameof(DurationText));
+            }
+        };
     }
 
-    private void ExecuteScrollToBottom(object? _) {
-        ScrollState.IsScrollRequested = true;
-    }
-
-
-    private bool CanScrollToBottom(object? _) {
-        return !ScrollState.IsAtBottom;
-    }
+    private void ExecuteScrollToBottom(object? _) => ScrollState.IsScrollRequested = true;
+    private bool CanScrollToBottom(object? _) => !ScrollState.IsAtBottom;
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
