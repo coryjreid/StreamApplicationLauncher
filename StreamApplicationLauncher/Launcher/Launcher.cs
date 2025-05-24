606da4618f180baddd4f740c477c5ac91db656ab
Cory Reid <11530109+coryjreid@users.noreply.github.com>
2025-05-24 08:14:45 -0600

    Further Flesh Out Launcher Implementation

--- StreamApplicationLauncher/Launcher/Launcher.cs
+++ StreamApplicationLauncher/Launcher/Launcher.cs
@@ -9,6 +9,7 @@ namespace StreamApplicationLauncher.Launcher;
 public class Launcher {
     private readonly LogManager _logManager;
     private readonly List<Program>? _programs;
+    private const int WindowWaitTimeoutSeconds = 10;
 
     public Launcher(LogManager logManager, string configPath) {
         _logManager = logManager;
@@ -29,16 +30,45 @@ public class Launcher {
     public void Run() {
         Thread.Sleep(2000);
         _logManager.Info($"Loaded {_programs!.Count} {(_programs!.Count == 1 ? "program" : "programs")} for launch");
-        _logManager.Info("The Run() method was called");
 
         int programsCount = _programs.Count;
         int currentProgram = 0;
-        foreach (Program program in _programs) {
-            _logManager.Info($"({++currentProgram}/{programsCount}) Starting application {program.Name}");
-            int result = AutoItX.Run(program.Process.ExecutablePath, program.Process.WorkingDirectory);
-            int errorCode = AutoItX.ErrorCode();
-            if (result == 1 || errorCode == 1) {
-                _logManager.Critical($"Application {program.Name} failed to start");
+        foreach ((string programName, Process process, Window window) in _programs) {
+            // Perform some validation
+            if (window is { CloseOnOpen: true, HideOnOpen: true }) {
+                _logManager.Critical("CloseOnOpen and HideOnOpen are mutually exclusive; Aborting");
+                return;
+            }
+
+            bool skipWindowHandle = false;
+            if (!string.IsNullOrWhiteSpace(window.Title) && window is { CloseOnOpen: false, HideOnOpen: false }) {
+                _logManager.Warning(
+                    $"{programName} has a window title defined but does not close or hide on open; Obtaining a window handle will be skipped");
+                skipWindowHandle = true;
+            }
+
+            // Perform launch procedure
+            _logManager.Info($"({++currentProgram}/{programsCount}) Starting application {programName}");
+            int pid = AutoItX.Run(process.ExecutablePath, process.WorkingDirectory);
+            if (pid == 0 || AutoItX.ErrorCode() != 0) {
+                _logManager.Critical($"Application {programName} failed to start; Skipping");
+                continue;
+            }
+
+            if (!skipWindowHandle) {
+                _logManager.Info($"Waiting for {programName}'s window to exist (up to {WindowWaitTimeoutSeconds} seconds)");
+                int windowHandle = AutoItX.WinWait(window.Title, null, WindowWaitTimeoutSeconds);
+                _logManager.Info("Successfully obtained window handle");
+
+                if (window.HideOnOpen) {
+                    if (AutoItX.WinSetState(windowHandle, AutoItX.SW_HIDE) != 1) {
+                        _logManager.Error($"Failed to hide {programName}'s window; Continuing");
+                    }
+                } else if (window.CloseOnOpen) {
+                    if (AutoItX.WinClose(window.Title) != 1) {
+                        _logManager.Error($"Failed to close {programName}'s window; Continuing");
+                    }
+                }
             }
         }
     }
